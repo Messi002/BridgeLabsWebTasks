51845f9881ad901e4da13e63fba405ecce9e6353
Messi002 <rostandmessi2@gmail.com>
2022-08-01 15:54:32 +0100

    FrontEndTask

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import Login from './LogIn_Register_Logout/Login';
 
 function App() {
--- src/LogIn_Register_Logout/Login.jsx
+++ src/LogIn_Register_Logout/Login.jsx
@@ -1,5 +1,6 @@
 import React,{useState} from 'react'
 import { useNavigate } from "react-router";
+import img from '../Assets/img.PNG'
 import {
   Card,
   Col,
@@ -12,9 +13,38 @@ import {
 } from "react-bootstrap";
 
 const Login = () => {
+
+    const handleSubmit= () =>{
+
+    }
   return (
-    <div>Login</div>
-  )
+<Container>
+    <Row className="mt-5 justify-content-md-center"> 
+        <Col sm={9}>
+            <Card.Body>
+                <Row>
+                    <Col sm={6}>
+                        <Image src={img} className=' w-100 h-100'/>
+                    </Col>
+                    <Col className="text-center">
+                        <Row>
+                        <h3>Login</h3>
+
+                        </Row>
+                        <Row>
+                            <Form onSubmit={handleSubmit} method="POST">
+                                <Form.Group>
+                                    <Form.Label>Email</Form.Label>
+                                    <Form.Control type="email" name="email" maxLength="30" />
+                                </Form.Group>
+                            </Form>
+                        </Row>
+                    </Col>
+                </Row>
+            </Card.Body>
+        </Col>
+    </Row>
+</Container>  )
 }
 
 export default Login
