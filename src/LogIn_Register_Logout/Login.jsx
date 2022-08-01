5f439f43890930357d68365928ec787f1cb2262e
Messi002 <rostandmessi2@gmail.com>
2022-08-01 16:20:33 +0100

    FrontEndTask

--- src/LogIn_Register_Logout/Login.jsx
+++ src/LogIn_Register_Logout/Login.jsx
@@ -13,13 +13,16 @@ import {
 } from "react-bootstrap";
 
 const Login = () => {
+ let navigate = useNavigate;
+ const [showing, setShowing]=useState(false);
+ const [msg, setSMsg]=useState([]);
 
     const handleSubmit= () =>{
 
     }
   return (
 <Container>
-    <Row className="mt-5 justify-content-md-center"> 
+    <Row className="m-auto justify-content-center"> 
         <Col sm={9}>
             <Card.Body>
                 <Row>
@@ -32,16 +35,39 @@ const Login = () => {
 
                         </Row>
                         <Row>
-                            <Form onSubmit={handleSubmit} method="POST">
+                            <Form onSubmit={handleSubmit} method="POST" className="justifiy-content-between">
+                                {
+                                    msg.map((item)=>{
+                                     return   <p>
+                                            {item.error && item.error}
+                                        </p>
+                                    })
+                                }
                                 <Form.Group>
                                     <Form.Label>Email</Form.Label> <br />
                                     <Form.Control type="email" name="email" maxLength="30" />
                                 </Form.Group>
 
                                 <Form.Group>
-                                    <Form.Label>Email</Form.Label> <br />
-                                    <Form.Control type="email" name="email" maxLength="30" />
+                                    <Form.Label>Password</Form.Label> <br />
+                                    <Form.Control type="password" name="password" maxLength="30" />
                                 </Form.Group>
+
+                                <Button variant="primary"  onClick={()=> navigate('/register') }>
+                                    LogIn
+                                </Button>
+                                {
+                                    showing? (
+                                        <Button disabled style={{color:'green'}} >
+                                            <Spinner as="span" animation="border" role="status" aria-hidden="true" />
+                                            Loading...
+                                        </Button>
+                                    ) : (
+                                            <Button type="submit">
+                                                Login
+                                            </Button>
+                                    )
+                                }
                             </Form>
                         </Row>
                     </Col>
