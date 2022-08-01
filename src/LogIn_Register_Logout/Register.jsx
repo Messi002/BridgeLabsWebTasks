d681930c4cd663d8e83185d9fd21e9248eaa9bbc
Messi002 <rostandmessi2@gmail.com>
2022-08-01 18:33:12 +0100

    FrontEndTask

--- src/LogIn_Register_Logout/Register.jsx
+++ src/LogIn_Register_Logout/Register.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import img from '../Assets/img.PNG';
 
 import {
   Card,
@@ -19,7 +20,23 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div>
+        <Container>
+            <Row className="mt-5 mb-3 justify-content-center">
+                <Col sm={9}>
+                    <Card>
+                        <Card.Body>
+                            <Row>
+                                <Col sm={6}>
+                                    <Image src={img} clasName="w-100 h-100"/>
+                                </Col>
+                            </Row>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    </div>
   )
 }
 
