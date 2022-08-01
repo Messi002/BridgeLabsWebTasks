3270acd2519cdba5327df931367846b388bb5c58
Messi002 <rostandmessi2@gmail.com>
2022-08-01 21:44:50 +0100

    FrontEndTask

--- src/LogIn_Register_Logout/Register.jsx
+++ src/LogIn_Register_Logout/Register.jsx
@@ -7,6 +7,21 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
     const [isShowing, setIsShowing] = useState(false);
+    const [validated, setValidated] = useState(false);
+    const [user, setUser] = useState({})
+
+    const handleChange = (e) => {
+        const {name, value} = e.target;
+        setUser({...user,[name]:value});
+        console.log(user);
+        console.log(setUser);
+
+    }
+
+    const 
+
+
+
   return (
     <div className=" container mt-3 d-flex justify-content-center align-items-center" >
  <div className="row p-3" style={{border:"0.2px solid grey"}}>
@@ -17,7 +32,9 @@ const Register = () => {
 
  <div className="mt-4 col-sm-12 col-lg-6  justify-content-center text-center pb-2" >
     <h3 className="text-start">Registration</h3>
- <form class="row" onSubmit={handleUser} method="POST" validated={validated} noValidate>
+
+    {/* form begins here */}
+ <form class="row"  method="POST" validated={validated} noValidate>
         <div className="col-lg-12 col-sm-4 text-start" >
             <label for="inputFName" className="form-label">First Name</label>
             <input type="text" class="form-control" name="inputFName" required onChanged={(e)=> handleChange(e)}/>
@@ -25,22 +42,22 @@ const Register = () => {
 
         <div className="col-lg-12 col-sm-4 text-start">
             <label for="inputLName" className="form-label">Last Name</label>
-            <input type="text" class="form-control" name="inputLName"/>
+            <input type="text" class="form-control" name="inputLName" required onChanged={(e)=> handleChange(e)}/>
         </div>
 
         <div className="col-lg-12 col-sm-4 text-start">
             <label for="phoneNum" className="form-label">Phone</label>
-            <input type="number" class="form-control" name="phoneNum"/>
+            <input type="text" class="form-control" name="phoneNum" required onChanged={(e)=> handleChange(e)}/>
         </div>
 
         <div className="col-lg-12 col-sm-4 text-start">
             <label for="email" className="form-label">Email</label>
-            <input type="email" class="form-control" name="email"/>
+            <input type="email" class="form-control" name="email" required onChanged={(e)=> handleChange(e)}/>
         </div>
 
         <div className="col-lg-12 col-sm-4 text-start">
             <label for="password" className="form-label">Password</label>
-            <input type="password" class="form-control" name="password"/>
+            <input type="password" class="form-control" name="password" required onChanged={(e)=> handleChange(e)}/>
         </div>
 
         <div className="col-lg-12 col-sm-4 d-flex mt-3 justify-content-around">
