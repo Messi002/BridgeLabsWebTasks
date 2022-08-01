9e70a3c22075df89bb24ef5e0c010ca54b2ff1e1
Messi002 <rostandmessi2@gmail.com>
2022-08-01 20:24:48 +0100

    FrontEndTask

--- src/LogIn_Register_Logout/Register.jsx
+++ src/LogIn_Register_Logout/Register.jsx
@@ -6,19 +6,51 @@ import "react-toastify/dist/ReactToastify.css";
 
 const Register = () => {
   return (
-    <div className="Contaier d-flex justify-content-center align-items-center" style={{width:"100vw", height:"100vh"}}>
- <div className="row p-2" style={{border:"0.2px solid grey"}}>
+    <div className=" container mt-3 d-flex justify-content-center align-items-center" >
+ <div className="row p-3" style={{border:"0.2px solid grey"}}>
 
- <div className="bg-danger col-sm-12 col-lg-6  justify-content-center text-center">
-<img src={img} className="img-fluid w-100 h-100" alt="Loading..."/>
+ <div className="bg-danger col-sm-12 col-lg-6 d-flex justify-content-center text-center">
+<img src={img} className="img-fluid" alt="Loading..."/>
  </div>
 
- <div className="bg-warning col-sm-12 col-lg-6  justify-content-center text-center">
- <form class="row g-3">
-        <div className="col-lg-12 col-sm-4 text-start">
+ <div className="bg-warning col-sm-12 col-lg-6  justify-content-center text-center pb-2" >
+    <h3 className="text-start">Registration</h3>
+ <form class="row">
+        <div className="col-lg-12 col-sm-4 text-start" >
             <label for="inputFName" className="form-label">First Name</label>
             <input type="text" class="form-control" name="inputFName"/>
         </div>
+
+        <div className="col-lg-12 col-sm-4 text-start">
+            <label for="inputLName" className="form-label">Last Name</label>
+            <input type="text" class="form-control" name="inputLName"/>
+        </div>
+
+        <div className="col-lg-12 col-sm-4 text-start">
+            <label for="phoneNum" className="form-label">Phone</label>
+            <input type="number" class="form-control" name="phoneNum"/>
+        </div>
+
+        <div className="col-lg-12 col-sm-4 text-start">
+            <label for="email" className="form-label">Email</label>
+            <input type="email" class="form-control" name="email"/>
+        </div>
+
+        <div className="col-lg-12 col-sm-4 text-start">
+            <label for="password" className="form-label">Password</label>
+            <input type="password" class="form-control" name="password"/>
+        </div>
+
+        <div className="col-lg-12 col-sm-4">
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+           
+        </div>
+
 
       </form>
  </div>
