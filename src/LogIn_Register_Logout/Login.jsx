07293ff0da3a5f0864e2fef215db37a38c0e5a2a
Messi002 <rostandmessi2@gmail.com>
2022-08-01 15:20:12 +0100

    FrontEndTask

---
+++ src/LogIn_Register_Logout/Login.jsx
@@ -0,0 +1,20 @@
+import React,{useState} from 'react'
+import { useNavigate } from "react-router";
+import {
+  Card,
+  Col,
+  Container,
+  Form,
+  Row,
+  Button,
+  Image,
+  Spinner,
+} from "react-bootstrap";
+
+const Login = () => {
+  return (
+    <div>Login</div>
+  )
+}
+
+export default Login
